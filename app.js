69f84f16e729777e48e2cbad3f5ea9759196d176
Thomas Weber <kontakt@weber-thomas.at>
2018-11-05 23:16:13 +0100

    added json users

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-const Crawler = require('./lib/crawler');
+const fs = require('fs');
 const CronJob = require('cron').CronJob;
-const FlatChecker = require('./lib/flatchecker');
-const flatChecker = new FlatChecker();
-const logErr = require('./lib/logger').logErr;
 
-const users = require('./users').users;
+const Crawler = require('./lib/crawler');
+const FlatChecker = require('./lib/flatchecker');
+const logErr = require('./lib/logger').logErr;
+const User = require('./lib/user');
+const flatChecker = new FlatChecker();
 
 if (process.env.NODE_ENV == 'dev') {
   const server = require('./tests/www');
@@ -23,20 +24,33 @@ startCron('0 */5 8-19 * * 1-5');
 
 async function startCrawl(callback) {
   const crawler = new Crawler();
-  let flats = await crawler.crawl();
-  let newFlats = await flatChecker.compare(flats);
+  const users = [];
 
-  if (newFlats.length > 0) {
-    for (let user of users) {
-      user.alert(newFlats);
+  fs.readFile('./users.json', async (err, data) => {
+    if (err) throw err;
+
+    let usersJSON = JSON.parse(data);
+
+    for (let key in usersJSON) {
+      let user = new User(usersJSON[key].name, usersJSON[key].email, usersJSON[key].filter);
+      users.push(user);
     }
-  }
 
-  //to wait till crawl is finished
-  if (process.env.NODE_ENV == 'dev') {
-    return callback();
-  }
+    let flats = await crawler.crawl();
+    let newFlats = await flatChecker.compare(flats);
 
+    if (newFlats.length > 0) {
+      for (let user of users) {
+        user.alert(newFlats);
+      }
+    }
+
+    //wait till crawl is finished
+    if (process.env.NODE_ENV == 'dev') {
+      callback();
+    }
+
+  });
 }
 
 function startCron(cronTime) {
@@ -50,7 +64,9 @@ function startCron(cronTime) {
     }, 1000);
   } else {
     const job = new CronJob(cronTime, () => {
-      startCrawl();
+      startCrawl().catch((err) => {
+        logErr(err);
+      });
     }, null, null, "Europe/Amsterdam");
     job.start();
   }
